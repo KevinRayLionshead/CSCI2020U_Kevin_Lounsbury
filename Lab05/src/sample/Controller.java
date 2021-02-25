0292f1e6ba1fd459080ec8d888eb5fd68c6e7f0f
SnugSiamang55 <Kevin.lounsbury@uoit.net>
2021-02-25 09:47:21 -0500

    Lab05 grade table

---
+++ Lab05/src/sample/Controller.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class Controller {
+
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn SSIDCol;
+    @FXML
+    private TableColumn assignmentsCol;
+    @FXML
+    private TableColumn midtermCol;
+    @FXML
+    private TableColumn finalExamCol;
+    @FXML
+    private TableColumn finalMarkCol;
+    @FXML
+    private TableColumn letterGradeCol;
+
+
+    private TableView<StudentRecord> students;
+
+    @FXML
+    public void initialize(){
+        SSIDCol.setCellValueFactory(new PropertyValueFactory<>("SSID"));
+        assignmentsCol.setCellValueFactory(new PropertyValueFactory<>("assignments"));
+        midtermCol.setCellValueFactory(new PropertyValueFactory<>("midterm"));
+        finalExamCol.setCellValueFactory(new PropertyValueFactory<>("finalExam"));
+        finalMarkCol.setCellValueFactory(new PropertyValueFactory<>("finalMark"));
+        letterGradeCol.setCellValueFactory(new PropertyValueFactory<>("letterGrade"));
+
+        tableView.setItems(DataSource.getAllMarks());
+
+    }
+
+}
